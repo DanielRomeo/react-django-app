309e21d2e2f3ee76dc46ca2f56cb61d041922f0e
danielromeo <danielromeo99@gmail.com>
2023-03-23 22:03:41 +0200

    able to import from modules

---
+++ backend/mybackendapp/api/information.py
@@ -0,0 +1,2 @@
+
+Persons = [{'name': 'Dan', 'age': 20}, {'name': 'Kim', 'age': 33}, {'name': 'Dan', 'age': 20}, {'name': 'Paul', 'age': 100}]
--- backend/mybackendapp/api/views.py
+++ backend/mybackendapp/api/views.py
@@ -5,12 +5,14 @@ from django.http.response import JsonResponse # json response
 from rest_framework.parsers import JSONParser
 from rest_framework import status
 
+from .information import  Persons
+
 @api_view(['GET', 'POST'])
 def getData(request):
     if request.method == 'GET':
 
-        person = [{'name': 'Dan', 'age': 20}, {'name': 'Kim', 'age': 33}, {'name': 'Dan', 'age': 20}, {'name': 'Paul', 'age': 100}]
-        return Response(person)
+        data = Persons
+        return Response(data)
     
     elif request.method == 'POST':
         person_data = JSONParser().parse(request)
